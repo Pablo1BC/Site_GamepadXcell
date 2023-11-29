dbf452c56f8ddb593c3b0e95433c25914b8ba4e6
Pablo Barros <97306182+Pablo1BC@users.noreply.github.com>
2023-11-29 18:29:51 -0300

    a

---
+++ server/imagens.js
@@ -0,0 +1,50 @@
+function carregarJSON() {
+    fetch("dados.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Não foi possível carregar o arquivo JSON");
+            }
+            return response.json();
+        })
+        .then(data => {
+            exibirImagens(data);
+        })
+        .catch(error => {
+            console.error("Erro ao carregar JSON:", error);
+        });
+}
+
+function exibirImagens(jsonData) {
+    try {
+        var links = jsonData.links;
+
+     
+          // Cria uma string para armazenar os links
+          var linksHTML = "";
+
+          for (var i = 0; i < links.length; i++) {
+              var link = links[i];
+              // Cria um ID único para cada imagem
+              var imgId = "img" + (i + 1);
+  
+              // Concatena os links na string
+              linksHTML += `
+                  <div id="${imgId}" class="img_div" onclick="abrirModal('${imgId}', '${link}')" >
+                      <img class="imgs" src='${link}' alt='img'>
+                  </div>
+              `;
+          }
+  
+          // Injeta a string com os links no elemento com id "content"
+          document.getElementById("content").innerHTML = linksHTML;
+  
+
+    } catch (error) {
+        console.error("Erro ao analisar JSON:", error);
+    }
+}
+
+// Chama a função para carregar o JSON quando a página carregar
+window.onload = carregarJSON;
+
+
